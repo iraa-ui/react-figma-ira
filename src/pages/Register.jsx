5fa20275b05b02c419c35f58106281a1d53e0f75
iraa-ui <irevaliawanti@gmail.com>
2024-12-13 13:26:41 +0700

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./../styles/register.css";
-
+import { Link } from 'react-router-dom';
 function Register() {
   const [formData, setFormData] = useState({
     username: "",
