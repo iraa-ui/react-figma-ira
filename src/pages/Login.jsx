fd6c4b3e5faacd3ffdef2ae0019fc1748c5d8a5d
iraa-ui <irevaliawanti@gmail.com>
2024-12-13 11:41:38 +0700

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,25 +1,34 @@
 import React from 'react';
 import '../styles/login.css'; // Pastikan file CSS ini benar
-import background from '/assets/img/bg1.jpg';
+import background from '/assets/img/bg1.jpg'; // Path harus sesuai dengan struktur proyek Anda
 
 const Login = () => {
   const handleLogin = () => {
-    window.location.href = '/dashboard'; // Fungsi untuk navigasi
+    window.location.href = '/dashboard'; // Navigasi ke dashboard
   };
 
   return (
-    <div className="wrapper">
-      <img src={background} alt="Logo" />
+    <div
+      className="wrapper"
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundAttachment: 'fixed',
+      }}
+    >
       <div className="login-page">
         <form>
           <h1>Silahkan Login</h1>
           <div className="input-box">
-            <input type="text" placeholder="Masukan Email" required />
+            <input type="email" placeholder="Masukan Email" required />
           </div>
           <div className="input-box">
             <input type="password" placeholder="Masukan Password" required />
           </div>
-          <button type="button" className="btn btn-dark" onClick={handleLogin}><a href="/dashboard"></a> Log in </button>
+          <button type="button" className="btn btn-dark" onClick={handleLogin}>
+            Log in
+          </button>
           <div className="register-link">
             <p>Don't have an account?</p>
             <a href="/Register">Register</a>
