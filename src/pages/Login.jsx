4c0a1dcd4cbd70520ee4ecdbdae0cfc3fbc3dc74
iraa-ui <irevaliawanti@gmail.com>
2024-12-13 16:02:53 +0700

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,38 +1,33 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 import '../styles/login.css'; // Pastikan file CSS ini benar
-import background from '/assets/img/bg1.jpg'; // Path harus sesuai dengan struktur proyek Anda
+import background from '/assets/img/bg1.jpg';
+
 
 const Login = () => {
   const handleLogin = () => {
-    window.location.href = '/dashboard'; // Navigasi ke dashboard
+    window.location.href = '/Login'; // Fungsi untuk navigasi
   };
 
   return (
-    <div
-      className="wrapper"
-      style={{
-        backgroundImage: `url(${background})`,
-      }}
-    >
-      <div className="login-page">
-        <form>
-          <h1>Silahkan Login</h1>
-          <div className="input-box">
-            <input type="email" placeholder="Masukan Email" required />
-          </div>
-          <div className="input-box">
-            <input type="password" placeholder="Masukan Password" required />
-          </div>
-          <button type="button" className="btn btn-dark" onClick={handleLogin}>
-            Log in
-          </button>
-          <div className="register-link">
-            <p>Don't have an account?</p>
-            <a href="/Register">Register</a>
-          </div>
-          <h4>XI PPLG 2</h4>
-        </form>
+    <div className="background-login">
+      <div className="wrapper">
+        <div className="login-page">
+          <form>
+            <h1>Silahkan Login</h1>
+            <div className="input-box">
+              <input type="text" placeholder="Masukan Email" required />
+            </div>
+            <div className="input-box">
+              <input type="password" placeholder="Masukan Password" required />
+            </div>
+            <button type="button" className="btn btn-dark" onClick={handleLogin}>Log in</button>
+            <div className="register-link">
+              <p>Don't have an account?</p>
+              <a href="/Register">Register</a>
+            </div>
+            <h4>XI PPLG 2</h4>
+          </form>
+        </div>
       </div>
     </div>
   );
